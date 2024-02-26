e280bcc3b60e89db6603042d0d242697250be158
Junaid Noor <junaidkhan97991@gmail.com>
2024-02-26 08:13:12 -0800

    routing

---
+++ 07_React-router/src/Components/Course/Course.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Course() {
+  return (
+    <>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    <p className='bg-red-400 w-full h-[50vh]'>Blog</p>
+    </>
+  )
+}
+
